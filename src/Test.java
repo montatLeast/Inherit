fdb539976c42a8e57158c62913f3ddaaa8bc8984
montatleast <314099319@qq.com>
2019-06-26 17:02:06 +0800

    老虎喵喵叫

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger(20, 110);
+        tiger.meowMeow();
+    }
+}
---
+++ src/Tiger.java
@@ -0,0 +1,6 @@
+public class Tiger extends Felidae{
+
+    public Tiger(int age, float weight) {
+        super(age, weight);
+    }
+}
